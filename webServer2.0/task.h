8f4521ce0eb33aee623dfac484b8881be9d99cff
niliushall <1459775963@qq.com>
2018-11-27 21:18:43 +0800

    Update task.h

--- webServer2.0/task.h
+++ webServer2.0/task.h
@@ -17,30 +17,36 @@
 #include <fcntl.h>
 #include <sys/sendfile.h>
 #include <sys/wait.h>
+#include <errno.h>
 using namespace std;
 
 const int buffer_size = 1024;
+
+void removefd( int epollfd, int fd );
 
 // 封装任务执行的类
 class Task {
 private:
     int accp_fd;  // 存储accept的返回值
+    int epoll_fd;
 
 public:
     Task() {}
-    Task(int fd) : accp_fd(fd) {}
-    ~Task() { close( accp_fd ); }
+    Task(int fd, int epoll) : accp_fd(fd), epoll_fd(epoll) {}
+    ~Task() { removefd( epoll_fd, accp_fd ); }
+    // Task(int fd, int epoll) : accp_fd(fd), epoll_fd(epoll) { cout << accp_fd << " start\n"; }
+    // ~Task() { removefd( epoll_fd, accp_fd ); cout << accp_fd << " close\n"; }
+
 
     void doit();  // 执行任务
-
     // 构造HTTP首部
-    const string construct_header( const int num, const string & info,
-                                        const string & type );
+    // void construct_header( char *response, const int num, const char * info,
+    //                                     const char * type );
 
     // 发送文件
-    int send_file( const string & filename, const string & type, 
-                        const int num = 200, const string & info = "OK" );
-    int deal_get( const string & uri );  // 处理GET请求
+    int send_file( const string & filename, const char *type, 
+                        int start, const int num = 200, const char *info = "OK" );
+    int deal_get( const string & uri, int start = 0 );  // 处理GET请求
     int deal_post( const string & uri, char *buf );  // 处理POST请求
     int get_size( const string & filename );  // 获取文件大小
 };
