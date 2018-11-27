4d4635ca717f08a21ea898a2023d4b163fde2dcc
niliushall <1459775963@qq.com>
2018-11-27 21:17:14 +0800

    Update webServer.cpp

--- webServer2.0/webServer.cpp
+++ webServer2.0/webServer.cpp
@@ -11,7 +11,7 @@ int setnonblocking( int fd ) {
 void addfd( int epoll_fd, bool oneshot, int fd ) {
     epoll_event event;
     event.data.fd = fd;
-    event.events = EPOLLIN | EPOLLET;
+    event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
     if( oneshot ) {
         event.events |= EPOLLONESHOT;
     }
@@ -19,18 +19,17 @@ void addfd( int epoll_fd, bool oneshot, int fd ) {
     setnonblocking( fd );
 }
 
-void removefd( int epollfd, int fd ) {
-    epoll_ctl( epollfd, EPOLL_CTL_DEL, fd, 0 );
-    close( fd );
-}
-
 int WebServer::run() {
+    // 忽略SIGPIPE信号
+    signal( SIGPIPE, SIG_IGN );
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons( port );
     server_addr.sin_addr.s_addr = htonl( INADDR_ANY );
 
     sock_fd = socket( AF_INET, SOCK_STREAM, 0 );
-    if( sock_fd < 0 ) {
+    if( sock_fd < 0 ) {  
+
         cout << "socket error, line " << __LINE__ << endl;
         return -1;
     }
@@ -41,13 +40,13 @@ int WebServer::run() {
         return -1;        
     }
 
-    ret = listen( sock_fd, 5 );
+    ret = listen( sock_fd, 1024 );
     if( ret < 0 ) {
         cout << "listen error, line " << __LINE__ << endl;
         return -1;        
     }
 
-    ThreadPool<Task> threadpool(20);  // 创建线程池，并运行
+    ThreadPool<Task> threadpool(50);  // 创建线程池，并运行
 
     /* ThreadPool< Task > *threadpool;
     try {
@@ -57,21 +56,29 @@ int WebServer::run() {
         return -1;
     } */
 
-
-
     epoll_event events[max_event_num];
-    epoll_fd = epoll_create(5);
+    epoll_fd = epoll_create(1024);
 
     if( epoll_fd < 0 ) {
         cout << "epoll_create error, line: " << __LINE__ << endl;
         exit(-1);
     }
-    addfd( epoll_fd, false, sock_fd );
+
+    // addfd( epoll_fd, false, sock_fd );
+    //////修改addfd LT模式
+    epoll_event event;
+    event.data.fd = sock_fd;
+    event.events = EPOLLIN | EPOLLRDHUP;
+    epoll_ctl( epoll_fd, EPOLL_CTL_ADD, sock_fd, &event );
+    // setnonblocking( sock_fd );
+    //////
+
 
     while( true ) {
         ret = epoll_wait( epoll_fd, events, max_event_num, -1 );
+// printf("epoll_wait recv a connect %d\n", ret);
         if( ret < 0 ) {
-            cout << "epoll_wait error, line: " << __LINE__ << endl;
+            perror( "epoll_wait:" );
             return -1;
         }
         for( int i = 0; i < ret; i++ ) {
@@ -87,10 +94,13 @@ int WebServer::run() {
                 }
                 addfd(epoll_fd, true, conn_fd);
 
+            } else if( events[i].events & ( EPOLLRDHUP | EPOLLHUP | EPOLLERR ) ) {
+                // close( fd );
+                removefd( epoll_fd, fd );
             } else if( events[i].events & EPOLLIN ) {  //有数据写入
-                Task *task = new Task(fd);  // 新建任务
+                Task *task = new Task(fd, epoll_fd);  // 新建任务
                 threadpool.append( task );  // 添加任务
-
+// printf("append a task, %d\n", fd);
             } else {
                 cout << "\nother\n";
             }
